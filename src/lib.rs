a1ae94b237926818187d2d59d6540f38af432435
Griffone <thegriffones@gmail.com>
2021-09-06 21:50:23 +0200

    Switch to bitflags and implement asm testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-use std::fmt;
-
 mod utils;
 
 use wasm_bindgen::prelude::*;
@@ -11,19 +9,16 @@ use js_sys::Math;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[wasm_bindgen]
-#[repr(u8)]
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub enum Cell {
-    Dead = 0,
-    Alive = 1,
-}
+/// Bit-dense storage for cells.
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct BitStore(Vec<u8>);
 
 #[wasm_bindgen]
+#[derive(Debug, PartialEq, Eq)]
 pub struct Universe {
 	width: u32,
 	height: u32,
-	cells: Vec<Cell>,
+	cells: BitStore,
 }
 
 // Private helper methods
@@ -36,20 +31,32 @@ impl Universe {
 	}
 
 	/// Get the number of living neighbors around the provided cell.
-	fn live_neighbor_count(&self, x: u32, y: u32) -> u8 {
-		const OFFSETS: [u32; 3] = [u32::MAX, 0, 1];
+	fn live_neighbor_count(&self, x: u32, y: u32) -> u32 {
 		let mut count = 0;
-		for xo in OFFSETS {
-			for yo in OFFSETS {
+		for yo in [self.height - 1, 0, 1] {
+			for xo in [self.width - 1, 0, 1] {
 				if xo == 0 && yo == 0 {
 					continue;
 				}
 
-				count += self.cells[self.idx(x+xo, y+yo)] as u8;
+				let idx = self.idx(x.wrapping_add(xo), y.wrapping_add(yo));
+				count += self.cells.get(idx) as u32;
 			}
 		}
 
 		count
+	}
+
+	/// Get the cells of the universe.
+	pub fn cells(&self) -> &BitStore {
+		&self.cells
+	}
+
+	/// Set cells at provided coordinates to alive state.
+	pub fn set_cells<'a, T: IntoIterator<Item = &'a (u32, u32)>>(&mut self, cells: T) {
+		for &(x, y) in cells {
+			self.cells.set(self.idx(x, y), true)
+		}
 	}
 }
 
@@ -61,17 +68,17 @@ impl Universe {
 	/// True means 'alive', false means 'dead'.
 	pub fn tick(&mut self) {
 		let mut next = self.cells.clone();
+	
 		for y in 0..self.height {
 			for x in 0..self.width {
 				let idx = self.idx(x, y);
-				let cell = self.cells[idx];
 				let live_neighbor_count = self.live_neighbor_count(x, y);
 
-				next[idx] = match (cell, live_neighbor_count) {
-					(Cell::Alive, x) if x < 2 => Cell::Dead,
-					(Cell::Alive, x) if x > 3 => Cell::Dead,
-					(Cell::Dead, 3) => Cell::Alive,
-					(otherwise, _) => otherwise, // in other cases cell state remains the same
+				match (self.cells.get(idx), live_neighbor_count) {
+					(true, x) if x < 2 => next.set(idx, false),
+					(true, x) if x > 3 => next.set(idx, false),
+					(false, 3) => next.set(idx, true),
+					_ => (), // in other cases cell state remains the same
 				}
 			}
 		}
@@ -79,27 +86,23 @@ impl Universe {
 		self.cells = next;
 	}
 
-	/// Initialize a new universe with an interesting pattern.
-	pub fn new(width: u32, height: u32) -> Self {
-        let cells = (0..width * height)
-            .map(|_| if Math::random() > 0.5 {
-				Cell::Alive
-			} else {
-				Cell::Dead
-			})
-            .collect();
+	/// Create an empty universe.
+	pub fn empty(width: u32, height: u32) -> Self {
+		Self {
+			width,
+			height,
+			cells: BitStore::empty((width*height) as usize)
+		}
+	}
 
+	/// Initialize a new universe with an interesting pattern.
+	pub fn random(width: u32, height: u32) -> Self {
         Self {
             width,
             height,
-            cells,
+            cells: BitStore::random((width*height) as usize),
         }
 	}
-
-	/// Present the universe to the viewer.
-    pub fn render(&self) -> String {
-        self.to_string()
-    }
 
     /// Get the width of the universe in cells.
     pub fn width(&self) -> u32 {
@@ -112,22 +115,128 @@ impl Universe {
     }
 
     /// Get the pointer to cell data in the universe.
-    pub fn cells(&self) -> *const Cell {
+    pub fn cells_ptr(&self) -> *const u8 {
         self.cells.as_ptr()
     }
+
+	pub fn cells_size(&self) -> usize {
+		self.cells.size()
+	}
 }
 
+impl BitStore {
+	/// Test whether the provided bit is set.
+	pub fn get(&self, idx: usize) -> bool {
+		let mask = 1 << (idx % 8);
+		self.0[idx/8] & mask == mask 
+	}
 
-impl fmt::Display for Universe {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for line in self.cells.as_slice().chunks(self.width as usize) {
-            for &cell in line {
-                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
-                write!(f, "{}", symbol)?;
-            }
-            write!(f, "\n")?;
-        }
+	/// Set the provided bit to given state.
+	pub fn set(&mut self, idx: usize, val: bool) {
+		let mask = 1 << (idx % 8);
+		if val {
+			self.0[idx/8] |= mask
+		} else {
+			self.0[idx/8] &= !mask
+		}
+	}
 
-        Ok(())
-    }
+	/// Get the number of bytes the data occupies.
+	pub fn size(&self) -> usize {
+		self.0.len()
+	}
+
+	pub fn as_ptr(&self) -> *const u8 {
+		self.0.as_ptr()
+	}
+
+	/// Generate a randomly filled BitStore with at least the provided bit count.
+	pub fn random(length: usize) -> Self {
+		let rounding = (length % 8 != 0) as usize;
+		let max = length / 8 + rounding;
+		// NOTE: a more efficient variant would be to invoke Math::random() for every 50ish bits
+		// as it's the most computationally expensive operation during initialization.
+		Self((0..max)
+			.map(|_| (Math::random() * 256.0) as u8)
+			.collect()
+		)
+	}
+
+	/// Create an empty bitstore with at least provided bit count.
+	pub fn empty(length: usize) -> Self {
+		let rounding = (length % 8 != 0) as usize;
+		let max = length / 8 + rounding;
+		Self((0..max).map(|_| 0).collect())
+	}
+}
+
+impl std::fmt::Display for Universe {
+	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+		for y in 0..self.height {
+			for x in 0..self.width {
+				if self.cells.get(self.idx(x, y)) {
+					write!(f, "X")?;
+				} else {
+					write!(f, "-")?;
+				}
+			}
+			write!(f, "\n")?;
+		}
+
+		Ok(())
+	}
+}
+
+#[cfg(test)]
+mod test {
+	use super::*;
+
+	#[test]
+	fn test_bitstore() {
+		let mut store = BitStore::empty(2);
+	
+		assert_eq!(&store.0, &[0]);
+	
+		store.set(0, true);
+		assert_eq!(&store.0, &[1]);
+	
+		store.set(1, true);
+		assert_eq!(&store.0, &[3]);
+	
+		store.set(0, false);
+		assert_eq!(&store.0, &[2]);
+	}
+	
+	#[test]
+	fn test_neighbor_count() {
+		// leave an empty row and column to avoid wrapping artifacts
+		let mut universe = Universe::empty(4, 4);
+		universe.set_cells(&[(1, 0), (1, 1), (1, 2)]);
+
+		assert_eq!(universe.live_neighbor_count(0, 0), 2);
+		assert_eq!(universe.live_neighbor_count(1, 0), 1);
+		assert_eq!(universe.live_neighbor_count(2, 0), 2);
+		
+		assert_eq!(universe.live_neighbor_count(0, 1), 3);
+		assert_eq!(universe.live_neighbor_count(1, 1), 2);
+		assert_eq!(universe.live_neighbor_count(2, 1), 3);
+		
+		assert_eq!(universe.live_neighbor_count(0, 2), 2);
+		assert_eq!(universe.live_neighbor_count(1, 2), 1);
+		assert_eq!(universe.live_neighbor_count(2, 2), 2);
+
+		// check wrapping neighbors
+		assert_eq!(universe.live_neighbor_count(1, 3), 2);
+	}
+
+	#[test]
+	fn test_get_cell() {
+		const CELLS: &[(u32, u32)] = &[(1,2), (2,3), (3,1), (3,2), (3,3)];
+		let mut universe = Universe::empty(6, 6);
+		universe.set_cells(CELLS);
+
+		for &(x, y) in CELLS {
+			assert!(universe.cells.get(universe.idx(x, y)));
+		}
+	}
 }
--- tests/web.rs
+++ tests/web.rs
@@ -2,12 +2,57 @@
 
 #![cfg(target_arch = "wasm32")]
 
+extern crate wasm_game_of_life;
 extern crate wasm_bindgen_test;
+
 use wasm_bindgen_test::*;
+use wasm_game_of_life::Universe;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
+#[cfg(test)]
+pub fn input_spaceship() -> Universe {
+	let mut universe = Universe::empty(6, 6);
+	universe.set_cells(&[(1,2), (2,3), (3,1), (3,2), (3,3)]);
+	universe
+}
+
+#[cfg(test)]
+pub fn expected_spaceship() -> Universe {
+	let mut universe = Universe::empty(6, 6);
+	universe.set_cells(&[(2,1), (2,3), (3,2), (3,3), (4,2)]);
+	universe
+}
+
 #[wasm_bindgen_test]
-fn pass() {
-    assert_eq!(1 + 1, 2);
+pub fn test_trivial_tick() {
+	let mut universe = Universe::empty(1, 1);
+	universe.set_cells(&[(0, 0)]);
+	universe.tick();
+
+	assert_eq!(universe, Universe::empty(1, 1));
+}
+
+#[wasm_bindgen_test]
+pub fn test_cross_tick() {
+	let mut universe = Universe::empty(4, 4);
+	universe.set_cells(&[(1, 0), (1, 1), (1, 2)]);
+
+	let expected = {
+		let mut universe = Universe::empty(4, 4);
+		universe.set_cells(&[(0, 1), (1, 1), (2, 1)]);
+		universe
+	};
+
+	universe.tick();
+	assert_eq!(universe, expected)
+}
+
+#[wasm_bindgen_test]
+pub fn test_spaceship_tick() {
+	let mut input_universe = input_spaceship();
+	input_universe.tick();
+
+	let expected = expected_spaceship();
+	assert_eq!(input_universe, expected);
 }
